53d065945e9a7c131d7f299cf979a8686128cedc
PStang <paul@stacom.de>
2014-12-05 16:11:08 +0100

    added some classes for the game

---
+++ FurryLana/Engine/Entity/Floor.cs
@@ -0,0 +1,103 @@
+﻿using FurryLana.Engine.Entity.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurryLana.Engine.Entity
+{
+    class Floor : IEntity
+    {
+        /// <summary>
+        /// Gets the ID.
+        /// </summary>
+        /// <value>
+        /// The ID.
+        /// </value>
+        public int ID
+        {
+            get { return 0; }
+        }
+
+        /// <summary>
+        /// Gets the model.
+        /// </summary>
+        /// <value>
+        /// The model.
+        /// </value>
+        public Model.Interfaces.IModel Model { get; set; }
+
+        public BoundingBox BBox { get; set; }
+
+        public void Draw()
+        {
+            Model.Draw();
+        }
+
+        public void Update(int deltaTime)
+        {
+           
+        }
+
+        public void FrameSyncedUpdate(float deltaTime)
+        {
+           
+        }
+
+        /// <summary>
+        /// Init this resource. This method may not be called from the main thread as the initialization process is
+        /// multi threaded.
+        /// </summary>
+        public void Init()
+        {
+            //Model.Load();
+
+        }
+
+        /// <summary>
+        /// Gets the init jobs.
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns>
+        /// The init jobs.
+        /// </returns>
+        public List<Action> GetInitJobs(List<Action> list)
+        {
+            list.Add(Init);
+            return list;
+        }
+
+        public void Load()
+        {
+            Loaded = false;
+
+        }
+
+        public List<Action> GetLoadJobs(List<Action> list, EventHandler reloader)
+        {
+            list.Add(Load);
+            NeedsLoad = reloader;
+            return list;
+        }
+
+        public void Destroy()
+        {
+            Model.Destroy();
+        }
+
+        public bool Loaded { get; protected set; }
+
+        public EventHandler NeedsLoad
+        { get; set; }
+
+        public Pencil.Gaming.MathUtils.Vector3 Position { get; set; }
+
+        public Pencil.Gaming.MathUtils.Vector3 Rotation
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string Name { get; set; }
+    }
+}
---
+++ FurryLana/Engine/Entity/Interfaces/BoundingBox.cs
@@ -0,0 +1,25 @@
+﻿using Pencil.Gaming.MathUtils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FurryLana.Engine.Entity.Interfaces
+{
+   public class BoundingBox
+    {
+        Vector3 position;
+        Vector3 size;
+        public BoundingBox(Vector3 position, Vector3 size) 
+        {
+            this.position = position;
+            this.size = size;
+        }
+        public BoundingBox(Vector3 position)
+        {
+            this.position = position;
+            this.size = new Vector3(1, 1, 1);
+        }
+
+    }
+}

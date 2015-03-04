3a7b6525b27fb2b9abe2948fc13bc10c5f65b56b
Martin Koppehel <psl.kontakt@gmail.com>
2015-03-04 16:27:23 +0100

    fix

--- FreezingArcher/Engine/Messaging/MessageID.cs
+++ FreezingArcher/Engine/Messaging/MessageID.cs
@@ -6,9 +6,21 @@
     /// </summary>
     public enum MessageId
     {
+        /// <summary>
+        /// The entity moved.
+        /// </summary>
         EntityMoved = 1,
+        /// <summary>
+        /// The register graphics resource.
+        /// </summary>
         RegisterGraphicsResource = 2,
+        /// <summary>
+        /// The content of the generate.
+        /// </summary>
         GenerateContent = 3,
+        /// <summary>
+        /// The generate terrain request.
+        /// </summary>
         GenerateTerrainRequest = 4,
     }
 }

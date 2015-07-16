05354dc681c6a3929233c8054363297233dc8aea
Fin Christensen <christensen.fin@gmail.com>
2015-07-16 22:45:37 +0200

    added threaded update for physicsmanager

--- FreezingArcher/Engine/Core/PhysicsManager.cs
+++ FreezingArcher/Engine/Core/PhysicsManager.cs
@@ -23,24 +23,45 @@
 using System;
 using Jitter;
 using Jitter.Collision;
+using System.Threading;
+using FreezingArcher.Output;
 
 namespace FreezingArcher.Core
 {
+    /// <summary>
+    /// This enum describes the collision system used by the physics engine.
+    /// </summary>
     public enum CollisionSystem
     {
+        /// <summary>
+        /// The sweep and prune collision system.
+        /// </summary>
         SweepAndPrune,
+        /// <summary>
+        /// The persistent sweep and prune collision system.
+        /// </summary>
         PersistentSweepAndPrune,
+        /// <summary>
+        /// The brute collision system.
+        /// </summary>
         Brute
     }
 
+    /// <summary>
+    /// This class manages a physics instance of one scene.
+    /// </summary>
     public sealed class PhysicsManager
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FreezingArcher.Core.PhysicsManager"/> class.
+        /// </summary>
+        /// <param name="collisionSystem">Collision system.</param>
         public PhysicsManager(CollisionSystem collisionSystem = CollisionSystem.SweepAndPrune)
         {
+            physicsThread = new Thread(run);
             this.collisionSystem = collisionSystem;
 
             Jitter.Collision.CollisionSystem system;
-
             switch (collisionSystem)
             {
                 case CollisionSystem.Brute:
@@ -55,10 +76,15 @@ namespace FreezingArcher.Core
             }
 
             World = new World(system);
+            physicsThread.Start();
         }
 
         CollisionSystem collisionSystem;
 
+        /// <summary>
+        /// Gets or sets the collision system.
+        /// </summary>
+        /// <value>The collision system.</value>
         public CollisionSystem CollisionSystem
         {
             get
@@ -85,17 +111,61 @@ namespace FreezingArcher.Core
             }
         }
 
-        public World World { get; private set; }
+        readonly Thread physicsThread;
+        readonly AutoResetEvent updateEvent = new AutoResetEvent(false);
+        TimeSpan timeStamp;
+        bool running;
+        bool physicsUpdateRunning = false;
 
-        public void Update(TimeSpan timeStamp)
+        void run()
         {
-            World.Step((float) timeStamp.TotalSeconds, true);
+            running = true;
+            while(running)
+            {
+                updateEvent.WaitOne();
+                physicsUpdateRunning = true;
+                World.Step((float) timeStamp.TotalSeconds, true);
+                physicsUpdateRunning = false;
+            }
         }
 
+        /// <summary>
+        /// Gets the physics world.
+        /// </summary>
+        /// <value>The world.</value>
+        public World World { get; private set; }
+
+        /// <summary>
+        /// Update physics with the specified time stamp.
+        /// </summary>
+        /// <param name="timeStamp">Time stamp.</param>
+        public void Update(TimeSpan timeStamp)
+        {
+            if (physicsUpdateRunning)
+            {
+                Logger.Log.AddLogEntry(LogLevel.Warning, "PhysicsManager",
+                    "A physics update is already running - ignoring update request...\n" +
+                    "A possible reason could be too many objects in the scene, the update cycle is too fast or your " +
+                    "computer is too slow."
+                );   
+            }
+            else
+            {
+                this.timeStamp = timeStamp;
+                updateEvent.Set();
+            }
+        }
+
+        /// <summary>
+        /// Destroy this instance.
+        /// </summary>
         public void Destroy ()
         {
+            running = false;
             World.Clear();
+            updateEvent.Close();
+            updateEvent.Dispose();
+            physicsThread.Abort();
         }
     }
 }
-
